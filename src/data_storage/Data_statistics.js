a2a51f94e81942dff449fa36b1ffee1553c7232e
hobbyluv <princewillchime43@gmail.com>
2020-08-27 15:41:35 +0100

    added table to hold the state data

---
+++ src/data_storage/Data_statistics.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Table from "./Table";
+import Form from "./Form";
+// import { MusicPlayerContext } from "./useContext";
+
+const Data_statistics = () => {
+  // const [data, setData, handleChange] = useContext(MusicPlayerContext);
+  return (
+    <div>
+      <Form />
+      <Table />
+    </div>
+  );
+};
+
+export default Data_statistics;
---
+++ src/data_storage/Table.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Thead from "./Thead";
+import Tbody from "./Tbody";
+
+const Table = () => {
+  return (
+    <table>
+      <Thead />
+      <Tbody />
+    </table>
+  );
+};
+
+export default Table;
