5abe60b67de7fd4ebae95ae05b5a035ea5cfdbcd
hobbyluv <princewillchime43@gmail.com>
2020-09-02 10:09:29 +0100

    Updates

---
+++ precache-manifest.ac3cb342d20387f12e8bc23d658bd1ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791f3a40205f1ec627bccb99d02c27b3",
+    "url": "/data_collection_react/index.html"
+  },
+  {
+    "revision": "fd52d3a5962b3fc2a136",
+    "url": "/data_collection_react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bfd7f603acb02af21d88",
+    "url": "/data_collection_react/static/js/2.803d9757.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/data_collection_react/static/js/2.803d9757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd52d3a5962b3fc2a136",
+    "url": "/data_collection_react/static/js/main.65940865.chunk.js"
+  },
+  {
+    "revision": "1aff787114d6c5dce830",
+    "url": "/data_collection_react/static/js/runtime-main.ca02172a.js"
+  }
+]);
